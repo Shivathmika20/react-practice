dd508d6caf5e974b8403b1d45e6b9a1c0dda463f
Shivathmika20 <yelurishivathmika@gmail.com>
2023-09-01 20:04:48 +0530

    edited

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { useState } from 'react'
 
-function Search(props) {
+function Search() {
     const [location,setLocation]= useState("");
-   
+    const [animal]=useState("");
   return (
     <div className='search'>
       {/* <h3>{props.brand}</h3> */}
@@ -12,6 +12,17 @@ function Search(props) {
             <input type="text" onChange={(e)=>{
                 setLocation(e.target.value);
             }} id='location' value={location} placeholder='Location'/>
+           </label>
+
+           <label htmlFor="location">Animal:
+            <select  id="animal" >
+              <option value="">{animal}</option>
+              <option value="Dog">Dog</option>
+              <option value="Cat">Cat</option>
+              <option value="Rabbit">Rabbit</option> 
+            </select>
+
+
            </label>
 
             
